30f2ce8521c00402d0cd64fcb4bab961294b11b8
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-06-25 16:40:49 -0700

    auth layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex min-h-screen">
+            <section className="justify-center hidden w-1/2 items-center bg-brand p-10 lg:flex xl:w-2/5">
+                <div className="flex flex-col max-h-[800px] max-w-[430px] justify-center space-y-12">
+                    <Image
+                        src="/assets/icons/logo-full.svg"
+                        alt="logo"
+                        width={224}
+                        height={82}
+                        className="h-auto"
+                    />
+                    <div className="space-y-5 text-white">
+                        <h1 className="h1">Manage your files the best way</h1>
+                        <p className="body-1">
+                            This is a place where you can store all your
+                            documents.
+                        </p>
+                    </div>
+                    <Image
+                        src="/assets/images/files.png"
+                        alt="files"
+                        width={342}
+                        height={342}
+                        className="transition-all hover:rotate-2 hover:scale-105"
+                    />
+                </div>
+            </section>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
