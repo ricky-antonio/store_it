cfda551922df497b1237ec5c83c6778e60951d74
Ricardo Monterrosa <48804047+ricky-antonio@users.noreply.github.com>
2025-07-08 14:35:19 -0700

    delete file

--- components/ActionDropDown.tsx
+++ components/ActionDropDown.tsx
@@ -24,7 +24,11 @@ import Link from "next/link";
 import { constructDownloadUrl } from "@/lib/utils";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { renameFile, updateFileUsers } from "@/lib/actions/file.actions";
+import {
+    deleteFile,
+    renameFile,
+    updateFileUsers,
+} from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
 import { FileDetails, ShareInput } from "./ActionsModalContent";
 
@@ -60,7 +64,12 @@ const ActionDropDown = ({ file }: { file: Models.Document }) => {
                     path,
                 }),
             share: () => updateFileUsers({ fileId: file.$id, emails, path }),
-            delete: () => console.log("delete"),
+            delete: () =>
+                deleteFile({
+                    fileId: file.$id,
+                    bucketFileId: file.bucketFileId,
+                    path,
+                }),
         };
 
         success = await actions[action.value as keyof typeof actions]();
@@ -105,6 +114,14 @@ const ActionDropDown = ({ file }: { file: Models.Document }) => {
                             onInputChange={setEmails}
                             onRemove={handleRemoveUser}
                         />
+                    )}
+                    {value === "delete" && (
+                        <p className="delete-confirmation">
+                            Are you sure you want to delete{" "}
+                            <span className="delete-file-name">
+                                {file.name}?
+                            </span>
+                        </p>
                     )}
                 </DialogHeader>
                 {["rename", "delete", "share"].includes(value) && (
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -148,3 +148,29 @@ export const updateFileUsers = async ({
         handleError(err, "Failed to rename file");
     }
 };
+
+export const deleteFile = async ({
+    fileId,
+    bucketFileId,
+    path,
+}: DeleteFileProps) => {
+    const { databases, storage } = await createAdminClient();
+
+    try {
+
+        const deletedFile = await databases.deleteDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            fileId,
+        );
+
+        if (deletedFile) {
+            await storage.deleteFile(appwriteConfig.bucketId, bucketFileId)
+        }
+
+        revalidatePath(path);
+        return parseStringify({status: "success"});
+    } catch (err) {
+        handleError(err, "Failed to rename file");
+    }
+};
