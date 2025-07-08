5a9a3e46c2b34dbe6393c8bef0de28923ac7ea25
Ricardo Monterrosa <48804047+ricky-antonio@users.noreply.github.com>
2025-07-08 16:55:41 -0700

    search files

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -64,7 +64,13 @@ export const uploadFile = async ({
     }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (
+    currentUser: Models.Document,
+    types: string[],
+    searchText: string,
+    sort: string,
+    limit?: number
+) => {
     const queries = [
         Query.or([
             Query.equal("owner", currentUser.$id),
@@ -72,10 +78,27 @@ const createQueries = (currentUser: Models.Document) => {
         ]),
     ];
 
+    if (types.length > 0) queries.push(Query.equal("type", types));
+    if (searchText) queries.push(Query.contains("name", searchText));
+    if (limit) queries.push(Query.limit(limit));
+
+    if (sort) {
+        const [sortBy, orderBy] = sort.split("-");
+
+        queries.push(
+            orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+        );
+    }
+
     return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({
+    types = [],
+    searchText = "",
+    sort = "$createdAt-desc",
+    limit,
+}: GetFilesProps) => {
     const { databases } = await createAdminClient();
 
     try {
@@ -83,7 +106,13 @@ export const getFiles = async () => {
 
         if (!currentUser) throw new Error("User not found");
 
-        const queries = createQueries(currentUser);
+        const queries = createQueries(
+            currentUser,
+            types,
+            searchText,
+            sort,
+            limit
+        );
 
         const files = await databases.listDocuments(
             appwriteConfig.databaseId,
@@ -123,7 +152,6 @@ export const renameFile = async ({
     }
 };
 
-
 export const updateFileUsers = async ({
     fileId,
     emails,
@@ -132,7 +160,6 @@ export const updateFileUsers = async ({
     const { databases } = await createAdminClient();
 
     try {
-
         const updatedFile = await databases.updateDocument(
             appwriteConfig.databaseId,
             appwriteConfig.filesCollectionId,
@@ -157,19 +184,18 @@ export const deleteFile = async ({
     const { databases, storage } = await createAdminClient();
 
     try {
-
         const deletedFile = await databases.deleteDocument(
             appwriteConfig.databaseId,
             appwriteConfig.filesCollectionId,
-            fileId,
+            fileId
         );
 
         if (deletedFile) {
-            await storage.deleteFile(appwriteConfig.bucketId, bucketFileId)
+            await storage.deleteFile(appwriteConfig.bucketId, bucketFileId);
         }
 
         revalidatePath(path);
-        return parseStringify({status: "success"});
+        return parseStringify({ status: "success" });
     } catch (err) {
         handleError(err, "Failed to rename file");
     }
